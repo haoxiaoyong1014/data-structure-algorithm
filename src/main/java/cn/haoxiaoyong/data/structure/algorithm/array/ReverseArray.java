009c527c90d9404fc057093b56bb368d34fb60d1
haoxiaoyong <hxyHelloWorld@163.com>
2020-12-08 16:20:31 +0800

    Add ReverseArray

--- src/main/java/cn/haoxiaoyong/data/structure/algorithm/array/ReverseArray.java
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/array/ReverseArray.java
@@ -5,30 +5,58 @@ package cn.haoxiaoyong.data.structure.algorithm.array;
  * @date created at 上午10:32 on 2020/12/8
  * @github https://github.com/haoxiaoyong1014
  * @blog www.haoxiaoyong.cn
- * 数组反转 空间复杂度O(1)
+ * 数组反转 空间复杂度O(1)/O(n)
  */
 public class ReverseArray {
 
-    public static void reverse(int n) {
+    //方式一
+    //空间复杂度O(1),时间复杂度O(n) （不考虑初始化数组大小）
 
+    public static void reverse(int n) {
+        //初始化数组
+        int[] arrs = initArr(n);
+
+        int j = 0;
+        for (; j < n; ++j) {
+            int temp;
+            temp = arrs[j];
+            arrs[j] = arrs[n - 1 - j];
+            arrs[n - 1 - j] = temp;
+        }
+    }
+
+    //方式二
+    //空间复杂度为O(n),时间复杂度O(n^2) （不考虑初始化数组大小）
+    public static void reverse() {
+        int n = 100;
+        //初始化数组
+        int[] arrs = initArr(n);
+        int[] ints = new int[n];
+
+        int i = 0;
+        for (; i < n; ++i) {
+            ints[i] = arrs[n - 1 - i];
+        }
+        int j = 0;
+        for (; j < n; ++j) {
+            arrs[j] = ints[j];
+        }
+
+    }
+
+    private static int[] initArr(int n) {
         int[] arr = new int[n];
 
         int i = 0;
         for (; i < n; ++i) {
             arr[i] = i;
         }
-        int j = 0;
-        for (; j < n; ++j) {
-            int temp;
-            temp = arr[j];
-            arr[j] = arr[n - 1 - j];
-            arr[n - 1 - j] = temp;
-        }
+        return arr;
     }
 
     public static void main(String[] args) {
-        reverse(100);
-
+        //reverse(100);
+        reverse();
     }
 
 }
