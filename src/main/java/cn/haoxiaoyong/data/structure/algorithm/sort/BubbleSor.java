b7a4c74372ad05cad45b79dfb6989c44e0eff3f1
haoxiaoyong <hxyHelloWorld@163.com>
2021-01-03 21:45:59 +0800

    Add optimizationBubbleSort

--- src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/BubbleSor.java
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/BubbleSor.java
@@ -28,18 +28,18 @@ public class BubbleSor {
     public static void optimizationBubbleSort(int[] arr, int n) {
 
         for (int i = 0; i < n; i++) {
-
+            // 提前退出冒泡循环的标志位
             boolean flag = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    flag = true;
+                    flag = true; // 表示有数据交换
                 }
             }
             if (!flag) {
-                break;
+                break;  // 没有数据交换，提前退出
             }
         }
         for (int k = 0; k < arr.length; k++)
