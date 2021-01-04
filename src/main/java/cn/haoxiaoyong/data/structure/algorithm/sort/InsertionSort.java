6e0c40ee275f8be259759b58d71b4ed28365095e
haoxiaoyong <hxyHelloWorld@163.com>
2021-01-04 16:01:26 +0800

    Add optimizationInsertionSort

--- src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/InsertionSort.java
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/InsertionSort.java
@@ -12,27 +12,8 @@ import java.util.Arrays;
 public class InsertionSort {
 
 
-    // 插入排序，a表示数组，n表示数组大小
-    public static void insertionSort(int[] a, int n) {
-        if (n <= 1) return;
-        for (int i = 1; i < n; ++i) { //无序的
-            int value = a[i];
-            int j = i - 1;
-            // 查找插入的位置
-            for (; j >= 0; --j) {  //有序的，拿无序的和有序的进行比较,满足就进行交换
-                if (a[j] > value) {
-                    a[j + 1] = a[j];  // 数据移动
-                } else {
-                    break;
-                }
-            }
-            a[j + 1] = value; // 插入数据
-        }
-        System.out.println(Arrays.toString(a));
-    }
-
     //个人觉得这个空间复杂度会高，而insertionSort方法 没有用中间变量
-    public static void insertion(int arr[], int n) {
+    public static void insertionSort(int arr[], int n) {
 
         for (int i = 1; i < n; i++) {
             int value = arr[i];
@@ -50,10 +31,29 @@ public class InsertionSort {
         }
         System.out.println(Arrays.toString(arr));
     }
+    // 插入排序，a表示数组，n表示数组大小
+    public static void optimizationInsertionSort(int[] a, int n) {
+        if (n <= 1) return;
+        for (int i = 1; i < n; ++i) { //无序的
+            int value = a[i];
+            int j = i - 1;
+            // 查找插入的位置
+            for (; j >= 0; --j) {  //有序的，拿无序的和有序的进行比较,满足就进行交换
+                if (a[j] > value) {
+                    a[j + 1] = a[j];  // 数据移动
+                } else {
+                    break;
+                }
+            }
+            a[j + 1] = value; // 插入数据
+        }
+        System.out.println(Arrays.toString(a));
+    }
+
 
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 1, 3, 2};
         insertionSort(arr, arr.length);
-        //insertion(arr,arr.length);
+        //optimizationInsertionSort(arr,arr.length);
     }
 }
