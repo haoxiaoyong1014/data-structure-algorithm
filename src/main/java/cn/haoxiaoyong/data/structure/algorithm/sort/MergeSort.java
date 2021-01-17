5432e508600c46ab8defe9fcbb22560a529c8122
haoxiaoyong <hxyHelloWorld@163.com>
2021-01-17 22:08:40 +0800

    Add QuickSort

--- src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/MergeSort.java
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/MergeSort.java
@@ -13,67 +13,60 @@ public class MergeSort {
 
     private static int[] tmps;
 
-    public static void mergeSort(int[] arr, int n) {
-
+    public static void mergeSort(int[] a, int n) {
+        tmps = new int[a.length];
+        //定义两个变量表示头元素lo和尾元素hi
         int lo = 0;
         int hi = n - 1;
-        //初始化一个临时数组；
-        tmps = new int[arr.length];
-        //分治，先分
-        sort(arr, lo, hi);
+        splitAndSort(a, lo, hi);
     }
 
-    private static void sort(int[] a, int lo, int hi) {
+    private static void splitAndSort(int[] a, int lo, int hi) {
         if (lo >= hi) {
             return;
         }
-
-        //对lo到hi之间的数据进行分为两个组
-        //int mid = lo+(hi-lo)/2;//   5,9  mid=7
+        //找到数组的中间位置
         int mid = (lo + hi) / 2;
+        //分解lo到mid的数据
+        splitAndSort(a, lo, mid);
+        //分解mid+1到n之间的数据
+        splitAndSort(a, mid + 1, hi);
 
-        //分别对每一组数据进行排序
-        sort(a, lo, mid);
-        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
 
-        //再把两个组中的数据进行归并
-        //merge(a, lo, mid, hi);
+    private static void merge(int[] a, int lo, int mid, int hi) {
+        //初始化3个指针 left:指向左子组的首节点，right:指向右子组的首节点 prov:指向临时数组的首节点
+        int left = lo;
+        int right = mid + 1;
+        int prov = lo;
 
-        //定义三个指针
-        int i = lo;
-        int p1 = lo;
-        int p2 = mid + 1;
-
-        //遍历，移动p1指针和p2指针，比较对应索引处的值，找出小的那个，放到辅助数组的对应索引处
-        while (p1 <= mid && p2 <= hi) {
-            //比较对应索引处的值
-            if (a[p1] <= a[p2]) {
-                tmps[i++] = a[p1++];
+        //比较大小，将小的放到临时数组中
+        while (left <= mid && right <= hi) {
+            if (a[left] < a[right]) {
+                tmps[prov++] = a[left++];
             } else {
-                tmps[i++] = a[p2++];
+                tmps[prov++] = a[right++];
             }
         }
-        // 下面两个循环只会走一个
-        //遍历，如果p1的指针没有走完，那么顺序移动p1指针，把对应的元素放到辅助数组的对应索引处
-        while (p1 <= mid) {
-            tmps[i++] = a[p1++];
+        // 看哪个子数组中还有数据，就将该子数组直接拷贝到临时数组中
+        while (left <= mid) {
+            tmps[prov++] = a[left++];
         }
-        //遍历，如果p2的指针没有走完，那么顺序移动p2指针，把对应的元素放到辅助数组的对应索引处
-        while (p2 <= hi) {
-            tmps[i++] = a[p2++];
+        while (right <= hi) {
+            tmps[prov++] = a[right++];
         }
-        //把辅助数组中的元素拷贝到原数组中
+
+        //将临时数组拷贝到原数组中
         for (int index = lo; index <= hi; index++) {
             a[index] = tmps[index];
         }
 
     }
-
-
     /*
-   对数组中，从lo到mid为一组，从mid+1到hi为一组，对这两组数据进行归并
-    */
-    private static void merge(int[] a, int lo, int mid, int hi) {
+      对数组中，从lo到mid为一组，从mid+1到hi为一组，对这两组数据进行归并
+       */
+    private static void merge1(int[] a, int lo, int mid, int hi) {
         //定义三个指针
         int i = lo;
         int p1 = lo;
---
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/QuickSort.java
@@ -0,0 +1,96 @@
+package cn.haoxiaoyong.data.structure.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author haoxiaoyong on 2021/1/17 下午 4:35
+ * e-mail: hxyHelloWorld@163.com
+ * github: https://github.com/haoxiaoyong1014
+ * Blog: www.haoxiaoyong.cn
+ * 快速排序
+ */
+public class QuickSort {
+
+    /**
+     * 把一个数组切分成两个子数组的基本思想：
+     * 1.找一个基准值，用两个指针分别指向数组的头部和尾部；
+     * 2.先从尾部向头部开始搜索一个比基准值小的元素，搜索到即停止，并记录指针的位置；
+     * 3.再从头部向尾部开始搜索一个比基准值大的元素，搜索到即停止，并记录指针的位置；
+     * 4.交换当前左边指针位置和右边指针位置的元素；
+     * 5.重复2,3,4步骤，直到左边指针的值大于右边指针的值停止。
+     */
+    private static void quickSort(int[] arr, int n) {
+
+        //用两个指针分别指向数组的头部和尾部
+        int head = 0;
+        int tail = n - 1;
+        sort(arr, head, tail);
+
+    }
+
+    private static void sort(int[] arr, int head, int tail) {
+        if (head >= tail) {
+            return;
+        }
+
+        //定义2个指针，一个是左指针，一个是右指针
+
+        int key = arr[head];
+        int left = head;
+        int right = tail + 1;
+        //定义一个基准数
+        while (true) {
+            //1: 从右开始向左移动，移动right指针，找到比基准值小的
+            while (/*less(key, arr[--right])*/key < arr[--right]) {
+                //从最右边移动到了最左边还是没有找到就跳出循环
+                if (right == head) {
+                    break;
+                }
+            }
+
+            //2:从左开始向右移动，移动left指针，找到比基准值大的
+            while (/*less(arr[++left], key)*/key > arr[++left]) {
+                //从最左边移动到了最右边还是没有找到就跳出循环
+                if (left == tail) {
+                    break;
+                }
+            }
+            //3:判断 left>=right,如果是，则证明元素扫描完毕，结束循环，如果不是，则交换元素即可
+            if (left >= right) {
+                break;
+            } else {
+                int tmp = arr[right];
+                arr[right] = arr[left];
+                arr[left] = tmp;
+                //exch(arr, left, right);
+            }
+        }
+
+
+        //4:最后将基准值进行移动
+        int tmp = arr[head];
+        arr[head] = arr[right];
+        arr[right] = tmp;
+        //交换分界值
+        //exch(arr, head, left);
+
+        //以基准值把一个数组切分成2个数组。然后分别对两个数组按照相同的规则进行切分
+        //左子组
+        sort(arr, head, right - 1);
+        //右子组
+        sort(arr, right + 1, tail);
+    }
+
+    public static void main(String[] args) {
+        int a[] = {6, 1, 2, 7,9, 3, 4, 5, 8};
+        quickSort(a, a.length);
+        System.out.println(Arrays.toString(a));
+    }
+
+    /*
+     比较v元素是否小于w元素
+  */
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+}
