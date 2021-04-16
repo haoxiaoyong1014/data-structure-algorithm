0b50c77981e2261949b1e447e890d3b4f5a24a95
haoxiaoyong <hxyHelloWorld@163.com>
2021-04-16 16:11:47 +0800

    once again sort

--- src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/BubbleSort.java
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/BubbleSort.java
@@ -1,5 +1,7 @@
 package cn.haoxiaoyong.data.structure.algorithm.sort;
 
+import java.util.Arrays;
+
 /**
  * @author haoxiaoyong
  * @date created at 下午1:24 on 2020/12/22
@@ -42,16 +44,74 @@ public class BubbleSort {
                 break;  // 没有数据交换，提前退出
             }
         }
-        for (int k = 0; k < arr.length; k++)
-            System.out.print(arr[k]);
+
+        System.out.print(Arrays.toString(arr));
     }
 
 
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 3, 2, 1};
         //bubbleSort(arr);
-        optimizationBubbleSort(arr, arr.length);
+        //optimizationBubbleSort(arr, arr.length);
+        //bubbleSort2(arr);
+        bubbleSortSuperior(arr);
     }
 
+
+    /**
+     * 20210416
+     * 冒泡排序_2刷
+     */
+
+    public static void bubbleSort2(int[] arr) {
+
+        int n = arr.length;
+        //外层表示循环的总次数
+        for (int i = 0; i < n; ++i) {
+            //n-i 表示已经循环了多少次，也就是已经有多少个有序的元素了，
+            // -1表示不能到最头，也就是倒数第二个元素和倒数第一个元素去比较就可以了
+            for (int j = 0; j < n - i - 1; ++j) {
+                //就行交换
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    /**
+     *  20210416
+     * 优化 冒泡排序_2刷
+     */
+
+    public static void bubbleSortSuperior(int[] arr) {
+
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+
+            //这个标志主要是为了所有的已经有序了之后就不需要在此进行循环了；
+            boolean flag = false;
+
+            for (int j = 0; j < n - i - 1; ++j) {
+
+                //比较交换
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j + 1];
+
+                    arr[j + 1] = arr[j];
+                    arr[j] = tmp;
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 
 }
--- src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/InsertionSort.java
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/sort/InsertionSort.java
@@ -31,6 +31,7 @@ public class InsertionSort {
         }
         System.out.println(Arrays.toString(arr));
     }
+
     // 插入排序，a表示数组，n表示数组大小
     public static void optimizationInsertionSort(int[] a, int n) {
         if (n <= 1) return;
@@ -53,7 +54,71 @@ public class InsertionSort {
 
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 1, 3, 2};
-        insertionSort(arr, arr.length);
-        //optimizationInsertionSort(arr,arr.length);
+        //insertionSort(arr, arr.length);
+        //optimizationInsertionSort(arr, arr.length);
+        //insertionSort2(arr);
+        insertionSortSuperior(arr);
+
+    }
+
+
+    /**
+     * 20210416
+     * 插入排序_2刷
+     * 1.分为已排序区和未排序区
+     * 2.初始默认第一个元素为已排序区，第一个元素之后的元素为未排序区
+     * 3，拿未排序的区域和已经排序的区域进行比较
+     */
+
+    public static void insertionSort2(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 1; i < n; ++i) {
+
+            for (int j = i - 1; j >= 0; --j) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 20210416
+     * 优化插入排序_2刷
+     * <p>
+     * 整体分成2个步骤，
+     * 1： 移动数据
+     * 2： 交换数据
+     */
+
+    public static void insertionSortSuperior(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; ++i) {
+
+            int value = arr[i];
+            int j = i - 1;
+            for (; j >= 0; --j) {
+
+                if (arr[j] > value) {
+                    //移动数据
+                    arr[j + 1] = arr[j];
+                } else {
+                    break;
+                }
+            }
+            //插入数据
+            arr[j + 1] = value;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
