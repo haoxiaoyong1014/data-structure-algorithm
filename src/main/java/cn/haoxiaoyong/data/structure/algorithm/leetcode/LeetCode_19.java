f57d4ffd2d10406770ad07a32455a6cc9920c912
haoxiaoyong <hxyHelloWorld@163.com>
2021-07-05 20:03:26 +0800

    Add LeetCode_19

--- src/main/java/cn/haoxiaoyong/data/structure/algorithm/leetcode/LeetCode_19.java
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/leetcode/LeetCode_19.java
@@ -1,5 +1,7 @@
 package cn.haoxiaoyong.data.structure.algorithm.leetcode;
 
+import javafx.beans.binding.ListBinding;
+
 /**
  * @author haoxiaoyong
  * @date created at 下午7:18 on 2021/7/5
@@ -10,7 +12,40 @@ package cn.haoxiaoyong.data.structure.algorithm.leetcode;
 public class LeetCode_19 {
 
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
 
+
+        if (head == null) {
+
+            return null;
+        }
+
+        ListNode curr = head;
+        if (n == 0) {
+            curr = curr.next;
+            return curr;
+        }
+
+        for (int i = 0; i < n ; i++) {
+            curr = curr.next;
+
+        }
+        curr.next = curr.next.next;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addAtIndex(0, 1);
+        myLinkedList.addAtIndex(1, 2);
+        myLinkedList.addAtIndex(2, 3);
+        myLinkedList.addAtIndex(3, 5);
+        ListNode reverse = removeNthFromEnd(myLinkedList.head.next, 3);
+        while (reverse != null) {
+            System.out.println(reverse.val);
+            reverse = reverse.next;
+        }
     }
 }
