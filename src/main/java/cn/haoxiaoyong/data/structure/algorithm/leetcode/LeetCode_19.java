3e2b653751df360bc28033834d93ce721a2df293
haoxiaoyong <hxyHelloWorld@163.com>
2021-07-05 19:19:44 +0800

    Add LeetCode_19

---
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/leetcode/LeetCode_19.java
@@ -0,0 +1,16 @@
+package cn.haoxiaoyong.data.structure.algorithm.leetcode;
+
+/**
+ * @author haoxiaoyong
+ * @date created at 下午7:18 on 2021/7/5
+ * @github https://github.com/haoxiaoyong1014
+ * @blog www.haoxiaoyong.cn
+ * 删除链表中第n个元素
+ */
+public class LeetCode_19 {
+
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+    }
+}
