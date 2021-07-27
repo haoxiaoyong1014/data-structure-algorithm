d62891d3b9274e9457cc406f6a794b891304792c
haoxiaoyong <hxyHelloWorld@163.com>
2021-07-27 23:09:54 +0800

    Add LeetCode_82

---
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/leetcode/LeetCode_82.java
@@ -0,0 +1,25 @@
+package cn.haoxiaoyong.data.structure.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author haoxiaoyong on 2021/7/27 下午 10:59
+ * e-mail: hxyHelloWorld@163.com
+ * github: https://github.com/haoxiaoyong1014
+ * Blog: www.haoxiaoyong.cn
+ */
+public class LeetCode_82 {
+
+    public static ListNode deleteDuplicates(ListNode head) {
+
+        Map<ListNode, Integer> map = new HashMap<>();
+
+        while (head != null) {
+            if (map.get(head) != null) {
+
+            }
+        }
+        return null;
+    }
+}
