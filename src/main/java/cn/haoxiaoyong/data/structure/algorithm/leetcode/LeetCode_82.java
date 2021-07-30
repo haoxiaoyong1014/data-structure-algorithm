3a583ef1e945542b2e191c0628fc03a483d92f57
haoxiaoyong <hxyHelloWorld@163.com>
2021-07-30 17:51:14 +0800

    Add LeetCode_83_2

--- src/main/java/cn/haoxiaoyong/data/structure/algorithm/leetcode/LeetCode_82.java
+++ src/main/java/cn/haoxiaoyong/data/structure/algorithm/leetcode/LeetCode_82.java
@@ -22,4 +22,57 @@ public class LeetCode_82 {
         }
         return null;
     }
+
+    public static ListNode deleteDuplicates3(ListNode head) {
+
+        //ListNode prev = new ListNode(0);
+        ListNode prev = head;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        ListNode gr = head.next.next;
+
+        while (gr != null) {
+            if (slow.val == fast.val) {
+                slow = slow.next;
+                fast = fast.next;
+                gr = gr.next;
+            }
+            if (fast.val == gr.val) {
+                slow = slow.next;
+                fast = fast.next;
+                gr = gr.next;
+            }
+            prev.next = fast;
+            prev = prev.next;
+            slow = slow.next;
+            fast = fast.next;
+            gr = gr.next;
+
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addAtIndex(0, -3);
+        myLinkedList.addAtIndex(1, -1);
+        myLinkedList.addAtIndex(2, 0);
+        myLinkedList.addAtIndex(3, 0);
+        myLinkedList.addAtIndex(4, 0);
+        myLinkedList.addAtIndex(5, 3);
+        myLinkedList.addAtIndex(6, 3);
+        myLinkedList.addAtIndex(7, 4);
+        myLinkedList.addAtIndex(8, 5);
+        /*myLinkedList.addAtIndex(9, 5);*/
+
+
+        ListNode reverse = deleteDuplicates3(myLinkedList.head.next);
+        while (reverse != null) {
+            System.out.println(reverse.val);
+            reverse = reverse.next;
+        }
+    }
+
 }
